8ede2c5710d07cbe6a72de40d96dc80036c89ec4
Johnny Magrippis <j@magrippis.com>
2023-11-07 15:13:00 +0100

    🎉  Chapter 1: scaffold project with SvelteKit wizard

---
+++ README.md
@@ -0,0 +1,17 @@
+# SvelteKit Dashboard
+
+## Setup
+
+To install dependencies:
+
+```bash
+pnpm install
+```
+
+## Developing
+
+Start a development server:
+
+```bash
+pnpm run dev
+```
---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<header />
+
+<h1>Welcome to SvelteKit Learn</h1>
+<p>
+	Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
+
+<style>
+	header {
+		background: #ff3e00;
+		height: 150px;
+	}
+</style>
