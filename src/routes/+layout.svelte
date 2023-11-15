6b1f4bb8d83c2d8c48890b0b8830255b659e9f60
Johnny Magrippis <j@magrippis.com>
2023-11-15 16:39:41 +0100

    🐛  the conditional should be outside of the promise!!!

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,11 +8,14 @@
 	import {onNavigate} from '$app/navigation'
 
 	onNavigate((navigation) => {
-		if (!document.startViewTransition) return
+		if (
+			!document.startViewTransition ||
+			navigation.from?.url.pathname === navigation.to?.url.pathname
+		)
+			return
 
 		return new Promise((resolve) => {
 			document.startViewTransition &&
-				navigation.from?.url.pathname !== navigation.to?.url.pathname &&
 				document.startViewTransition(async () => {
 					resolve()
 					await navigation.complete
@@ -22,27 +25,3 @@
 </script>
 
 <slot />
-
-<style>
-	@keyframes fade-out {
-		to {
-			opacity: 0;
-		}
-	}
-
-	@keyframes slide-from-right {
-		from {
-			transform: translateX(100%);
-		}
-	}
-
-	:root::view-transition-old(root) {
-		pointer-events: none;
-		animation: 300ms ease-out both fade-out;
-	}
-
-	:root::view-transition-new(root) {
-		pointer-events: none;
-		animation: 300ms ease-out both slide-from-right;
-	}
-</style>
