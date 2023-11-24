fd76bb3b3cd4ca46eacab813374390af8bdddb07
Johnny Magrippis <j@magrippis.com>
2023-11-24 16:02:01 +0100

    ♻️  run paraglide via the package.json script

--- src/lib/components/Card/Card.svelte
+++ src/lib/components/Card/Card.svelte
@@ -6,7 +6,7 @@
 <div class="rounded-xl bg-gray-50 p-2 shadow-sm">
 	<div class="flex p-4">
 		<slot />
-		<h3 class="ml-2 font-medium">{title}</h3>
+		<h3 class="ml-2 font-medium capitalize">{title}</h3>
 	</div>
 	<p
 		class="truncate rounded-xl bg-white px-4 py-8 text-center font-serif text-2xl"
