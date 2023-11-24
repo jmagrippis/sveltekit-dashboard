43b872c1ab6a14c71de4625ac2c49b94f38324b3
Johnny Magrippis <j@magrippis.com>
2023-11-24 17:36:53 +0100

    ✅  setup Playwright for e2e

---
+++ tests/navigation.spec.ts
@@ -0,0 +1,27 @@
+import {test, expect} from '@playwright/test'
+
+test('guest navigation smoke test', async ({page}) => {
+	await page.goto('/')
+
+	await expect(page).toHaveTitle(/Acme Dashboard/)
+	await expect(
+		page.getByRole('heading', {level: 1, name: 'Acme'}),
+	).toBeVisible()
+
+	// Navigate to the log in page
+	await page.getByRole('link', {name: 'Log in'}).click()
+
+	await expect(page).toHaveTitle(/Login/)
+	await expect(
+		page.getByRole('heading', {level: 1, name: 'Log in'}),
+	).toBeVisible()
+})
+
+test('guest gets redirected to login for protected routes', async ({page}) => {
+	await page.goto('/dashboard')
+
+	await expect(page).toHaveTitle(/Login/)
+	await expect(
+		page.getByRole('heading', {level: 1, name: 'Log in'}),
+	).toBeVisible()
+})
